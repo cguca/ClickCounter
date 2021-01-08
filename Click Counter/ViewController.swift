e93e404ac0dcf8804314e4709a40cf558248dbdd
Cary Guca <cary.guca@gmail.com>
2021-01-08 16:42:49 -0600

    Initial version of application

--- Click Counter/ViewController.swift
+++ Click Counter/ViewController.swift
@@ -9,11 +9,61 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var count = 0
+    var label: UILabel!
+    var secondLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        // label
+        let label = UILabel()
+        label.frame = CGRect(x: 100, y: 150, width: 60, height: 60)
+        label.text = "0"
+        view.addSubview(label)
+        self.label = label
+        
+        //second label
+        let secondLabel = UILabel()
+        secondLabel.frame = CGRect(x: 200, y: 150, width: 60, height: 60)
+        secondLabel.text = "0"
+        secondLabel.textColor = UIColor.red
+        view.addSubview(secondLabel)
+        self.secondLabel = secondLabel
+        
+        // button
+        let button = UIButton()
+        button.frame = CGRect(x: 100, y: 250, width: 60, height: 60)
+        button.setTitle("Click", for: .normal)
+        button.setTitleColor(UIColor.blue, for: .normal)
+        view.addSubview(button)
+        
+        button.addTarget(self, action: #selector(ViewController.incrementCount), for: UIControl.Event.touchUpInside)
+        
+        // decrement button
+        let decrementButton = UIButton()
+        decrementButton.frame = CGRect(x: 200, y: 250, width: 60, height: 60)
+        decrementButton.setTitle("Decr", for: .normal)
+        decrementButton.setTitleColor(UIColor.red, for: .normal)
+        view.addSubview(decrementButton)
+        
+        decrementButton.addTarget(self, action: #selector(ViewController.decrementCount), for: UIControl.Event.touchUpInside)
+        
     }
-
-
+    
+    @objc func incrementCount() {
+        self.count += 1
+        self.label.text = "\(self.count)"
+        self.secondLabel.text = "\(self.count)"
+        self.view.backgroundColor = UIColor.white
+    }
+    
+    @objc func decrementCount() {
+        self.count -= 1
+        self.label.text = "\(self.count)"
+        self.secondLabel.text = "\(self.count)"
+        
+        self.view.backgroundColor = UIColor.cyan
+    }
 }
 
